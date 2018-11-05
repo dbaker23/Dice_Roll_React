7ae7e0f02dc80788b48ef3765449e94db8f9c18d
Derek <derekjohnbaker23@gmail.com>
2018-11-05 13:54:16 -0600

    version 1.0

--- src/components/number_display.js
+++ src/components/number_display.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-const NumberDisplay = ({gameState}) => {  
-    return randomRoll(gameState);
-};
-
 const randomRoll = (gameState) => {
     if( gameState === 1 ) {
         return <p>Roll to play</p>;
     } else {
+        const playerRoll = Math.floor( Math.random() * 6 ) + 1;
+        const computerRoll = Math.floor( Math.random() * 6 ) + 1;
+        const winner = playerRoll > computerRoll ? 'You win!' : 'You lose!';
         return (
             <div>
-                <p>{Math.floor( Math.random() * 6 ) + 1}</p>                
+                <p>
+                    <span>You: {playerRoll}</span>                    
+                    <span>Computer: {computerRoll}</span>
+                </p>
+                <p>{winner}</p>
             </div>
         ); 
     }
 }
+
+const NumberDisplay = ({gameState}) => {  
+    return randomRoll(gameState);
+};
 
 export default NumberDisplay;
